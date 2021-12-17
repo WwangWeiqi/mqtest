f93c09414a1ad8ba72ed52673e38df6161b6f7fe
xiaoxi-- <xiaoxi0564@163.com>
2021-12-17 19:49:43 +0800

    fix

--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -12,10 +12,16 @@ function register(app, ...routers) {
 
 // All the APIs need to register here in order to be accessed
 function setRoutes(app): void {
+  let _routers = [
+    // demo,
+  ];
+
   app.use(`/api/${sysParameter.version}/queuemsg`, queuemsg);
 
   app.use(`/api/${sysParameter.version}`, index);
   app.use(`/api/${sysParameter.version}`, demo);
+
+  register(app, ..._routers);
 }
 
 export default setRoutes;
